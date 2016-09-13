76ec5d489fb11dd744978eb959cb45d84ff46c1f
Tiltedprogrammer <alekseytyurinspb@gmail.com>
2016-09-13 19:24:03 +0300

    Add files via upload

---
+++ replacement.py
@@ -0,0 +1,19 @@
+n = int(input("Введите длину первой части массива: "))
+m = int(input("Введите длину второй части массива: "))
+a = []
+for i in range(n+m):
+	a.append(input(Введите элемент массива))
+p = n-1 
+v = 0
+r = n-1
+for i in range(n//2):
+	v = a[i]
+	a[i]=a[p]
+	a[p]=v
+	p-=1
+for i in range(n,m//2+n):
+	v=a[i]
+	a[i]=a[r+n]
+	a[r+n]=v
+	r-=1
+print(a[::-1])	
