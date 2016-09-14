e0fa8bdc3e53353e2a02fbb411b138667fa2ae5f
Tiltedprogrammer <alekseytyurinspb@gmail.com>
2016-09-14 18:43:31 +0300

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -2,8 +2,8 @@ n = int(input("Вывод простых чисел до числа(включи
 vsechisla = [True for i in range(n)]
 for x in range(2,n):
 	for j in range(x * 2,n,x):
-		a[j] = False
-prostyechisla =[i for i in range(2,n) if a[i]]
-print(b)		
+		vsechisla[j] = False
+prostyechisla =[i for i in range(2,n) if vsechisla[i]]
+print(prostyechisla)		
 
 
